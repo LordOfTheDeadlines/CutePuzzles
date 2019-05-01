5166851b1cc3c1126719627c938dc1dd009defd0
LordOfTheDeadlines <49224188+LordOfTheDeadlines@users.noreply.github.com>
2019-05-01 15:32:09 +0500

    Add files via upload

---
+++ Cute Pazzle/Cute Pazzle/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Cute_Pazzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.counter = new System.Windows.Forms.Label();
+            this.startGame = new System.Windows.Forms.Button();
+            this.levels = new System.Windows.Forms.TrackBar();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.levelName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.levels)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(668, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(811, 405);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(665, 418);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Времени осталось:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(665, 451);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Попыток осталось:";
+            // 
+            // counter
+            // 
+            this.counter.AutoSize = true;
+            this.counter.Location = new System.Drawing.Point(811, 451);
+            this.counter.Name = "counter";
+            this.counter.Size = new System.Drawing.Size(32, 17);
+            this.counter.TabIndex = 6;
+            this.counter.Text = "100";
+            // 
+            // startGame
+            // 
+            this.startGame.Location = new System.Drawing.Point(668, 535);
+            this.startGame.Name = "startGame";
+            this.startGame.Size = new System.Drawing.Size(75, 23);
+            this.startGame.TabIndex = 7;
+            this.startGame.Text = "Начать";
+            this.startGame.UseVisualStyleBackColor = true;
+            this.startGame.Click += new System.EventHandler(this.startGame_Click);
+            // 
+            // levels
+            // 
+            this.levels.Location = new System.Drawing.Point(668, 257);
+            this.levels.Maximum = 2;
+            this.levels.Name = "levels";
+            this.levels.Size = new System.Drawing.Size(351, 56);
+            this.levels.TabIndex = 8;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Cute_Pazzle.Properties.Resources._75b53a69853253_5bbe0d80ddcb2;
+            this.pictureBox2.Location = new System.Drawing.Point(668, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(351, 102);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Cute_Pazzle.Properties.Resources.orig;
+            this.pictureBox1.Location = new System.Drawing.Point(36, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(520, 520);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(665, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(209, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Выберите уровень сложности:";
+            // 
+            // levelName
+            // 
+            this.levelName.AutoSize = true;
+            this.levelName.Location = new System.Drawing.Point(892, 221);
+            this.levelName.Name = "levelName";
+            this.levelName.Size = new System.Drawing.Size(56, 17);
+            this.levelName.TabIndex = 11;
+            this.levelName.Text = "Низкий";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 623);
+            this.Controls.Add(this.levelName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.levels);
+            this.Controls.Add(this.startGame);
+            this.Controls.Add(this.counter);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.levels)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label counter;
+        private System.Windows.Forms.Button startGame;
+        private System.Windows.Forms.TrackBar levels;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label levelName;
+    }
+}
+
---
+++ Cute Pazzle/Cute Pazzle/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cute_Pazzle
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            levels.Scroll += levelsScroll;
+        }
+        private DateTime levelTime;
+        private void levelsScroll(object sender, EventArgs e)
+        {
+            switch (levels.Value)
+            {
+                case 0:
+                    levelName.Text = "Низкий";
+                    counter.Text = "1000";
+                    levelTime = DateTime.Now.AddMinutes(20);
+                    break;
+                case 1:
+                    levelName.Text = String.Format("Средний");
+                    counter.Text = "400";
+                    levelTime = DateTime.Now.AddMinutes(15);
+                    break;
+                case 2:
+                    levelName.Text = String.Format("Сложный");
+                    counter.Text = "250";
+                    levelTime = DateTime.Now.AddMinutes(10);
+                    break;
+            }
+        }
+
+#region
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var dateTime = DateTime.Now;
+            if (dateTime < levelTime)
+            {
+                var timeSpan = (levelTime - dateTime);
+
+                label1.Text = string.Format("\r{0:00}:{1:00}:{2:00}", (int)timeSpan.TotalHours, (int)timeSpan.TotalMinutes,
+                                  (int)timeSpan.TotalSeconds);
+            }
+            else
+            {
+                button1_Click(null, EventArgs.Empty);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (!timer1.Enabled)
+            {
+                levelTime = DateTime.Now.AddMinutes(1);
+            }
+            timer1.Enabled = !timer1.Enabled;
+            button1.Text = timer1.Enabled ? "Stop" : "Start";
+        }
+        #endregion
+
+        private void startGame_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Timer
+    {
+       
+    }
+}
